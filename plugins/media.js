68ac4b94d6885782c8d73871680eed385b7a8781
SUHAID-BRO <99546226+SUHAID-BRO@users.noreply.github.com>
2024-01-25 19:14:34 +0530

    Update media.js

--- plugins/media.js
+++ plugins/media.js
@@ -114,6 +114,21 @@ X-Asena - X-Electra
 
 command(
   {
+    pattern: "mp3",
+    fromMe: isPrivate,
+    desc: "converts video/audio/voice to mp3",
+    type: "converter",
+  },
+  async (message, match, m) => {
+    if (!message.reply_message || (!message.reply_message.video && !message.reply_message.audio)) return await message.reply('*Reply at audio/voice/video!*')  
+    let buff = await m.quoted.download();
+    buff = await toAudio(buff, "mp3");
+     await message.sendMessage(buff, { mimetype: "audio/mpeg", quoted: message }, "audio");
+  }
+);
+  
+command(
+  {
     pattern: "mp4",
     fromMe: isPrivate,
     desc: "Changes sticker to Video",
