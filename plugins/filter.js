1ac7f02e09ab9a9536f15b02d9b33a30a6c3eb46
ʟᴏᴜɪs <157618892+Louis-X0@users.noreply.github.com>
2024-01-29 00:28:58 +0530

    Update filter.js

--- plugins/filter.js
+++ plugins/filter.js
@@ -4,7 +4,7 @@ const {
   deleteFilter,
   toggleFilter,
 } = require("../lib/database/filters");
-const { command, isPrivate, tiny } = require("../lib");
+const { Ezra, isPrivate, tiny } = require("../lib");
 
 const Lang = {
   FILTER_DESC:
@@ -19,7 +19,7 @@ const Lang = {
   DELETED: "*✅ The filter was successfully deleted!*",
 };
 
-command(
+Ezra(
   {
     pattern: "filter ?(.*)",
     fromMe: true,
@@ -56,7 +56,7 @@ command(
   }
 );
 
-command(
+Ezra(
   {
     pattern: "stop ?(.*)",
     fromMe: true,
@@ -77,7 +77,7 @@ command(
   }
 );
 
-command({ on: "text", fromMe: isPrivate }, async (message, match) => {
+Ezra({ on: "text", fromMe: isPrivate }, async (message, match) => {
   var filtreler = await getFilter(message.jid);
   if (!filtreler) return;
   filtreler.map(async (filter) => {
