ad2e43f49df5adc7b79820c7d5469140fae7d4b4
ʟᴏᴜɪs <157618892+Louis-X0@users.noreply.github.com>
2024-01-28 23:52:28 +0530

    Update fancy.js

--- plugins/fancy.js
+++ plugins/fancy.js
@@ -1,11 +1,11 @@
-const { Ezra, styletext, listall, tiny, isPrivate } = require("../lib/");
+const { command, styletext, listall, tiny, isPrivate } = require("../lib/");
 /* Copyright (C) 2022 X-Electra.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 X-Asena - X-Electra
 */
 
-Ezra(
+command(
   {
     pattern: "fancy",
     fromMe: isPrivate,
