da10cea61032cf75af7551a4a8dc7ebf8492a5bd
Aswin Sparky <113657908+A-S-W-I-N-S-P-A-R-K-Y@users.noreply.github.com>
2024-01-30 00:17:37 +0530

    Update and rename insta.js to _downloader.js

--- plugins/_downloader.js
+++ plugins/_downloader.js
@@ -1,6 +1,7 @@
 const { command , isPrivate , getBuffer} = require("../lib");
-const f = require("node-fetch");
-const config = require("../config");
+const fetch = require("node-fetch");
+const { SPARKY_API, CAPTION } = require("../config");
+
 
 
 command(
@@ -14,14 +15,14 @@ command(
     	
     	
    if (!match.includes("https://www.instagram"))return message.reply(`*Need instagram link!*`)
-      var ig = await (await fetch(`https://vihangayt.me/download/instagram?url=${match}`)).json();
+      var ig = await (await fetch(`${SPARKY_API}/downloader/igdl?url=${match}`)).json();
      var igdl = ig;
     let res = await getBuffer(`${igdl.data.data[0].url}`)
     let mtype = igdl.data.data[0].type;
       if(mtype == "video"){
-       await message.client.sendMessage(message.jid, { video :res ,  mimetype:"video/mp4", caption: (config.CAPTION)}, {quoted: message })
+       await message.client.sendMessage(message.jid, { video :res ,  mimetype:"video/mp4", caption: (CAPTION)}, {quoted: message })
       } else if(mtype == "image"){
-      await message.client.sendMessage(message.jid, { image :res ,  mimetype:"image/jpeg",caption: (config.CAPTION)}, {quoted: message })
+      await message.client.sendMessage(message.jid, { image :res ,  mimetype:"image/jpeg",caption: (CAPTION)}, {quoted: message })
 }
              
 });
